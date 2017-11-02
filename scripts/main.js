f4cece4f0207bee5aee827153856d9eae8679a8b
vinilmtuity <vinil.satrasala@mtuity.com>
2017-11-02 12:32:47 +0530

    initialcommit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
